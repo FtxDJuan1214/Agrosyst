134a87c52abfda822ca706e53b6caf218899124e
Juan Hernández <ftxd.jchernandezmail.com>
2020-05-17 20:18:28 -0500

    version que preparamos para subir al servidor

--- php/componentes/componentes_agroquimicos/tab_agroquimicos.php
+++ php/componentes/componentes_agroquimicos/tab_agroquimicos.php
@@ -30,50 +30,50 @@ $user =  $_SESSION['idusuario'];
     </thead>
     <tbody id="myTable">
         <?php 
-    $sql="SELECT agroquimicos.cod_agr, agroquimicos.cod_ins, agroquimicos.nom_agr, 
-    tipo_agroquimico.det_tag, agroquimicos.fun_agr, ingredientes_activos.des_iac, 
-    formulacion.nom_for, formulacion.sig_for, agroquimicos.pcr_agr, agroquimicos.pen_agr, 
-    agroquimicos.pro_agr, unidad_de_medida.des_unm, toxicidad.det_tox, 
-    ingredientes_activos.cod_iac, tipo_agroquimico.cod_tag, formulacion.cod_for,
-    agroquimicos.dos_agr, toxicidad.cod_tox,unidad_de_medida.cod_tum,
-    insumos.cod_ins, insumos.des_ins, agroquimicos.rap_agr
-    FROM agroquimicos
-    INNER JOIN tipo_agroquimico on agroquimicos.cod_tag=tipo_agroquimico.cod_tag
-    INNER JOIN ingredientes_activos on agroquimicos.cod_iac=ingredientes_activos.cod_iac
-    INNER JOIN formulacion on agroquimicos.cod_for=formulacion.cod_for
-    INNER JOIN insumos on agroquimicos.cod_ins=insumos.cod_ins
-    INNER JOIN unidad_de_medida ON insumos.cod_unm=unidad_de_medida.cod_unm
-    INNER JOIN toxicidad ON agroquimicos.cod_tox=toxicidad.cod_tox
-    WHERE (agroquimicos.cod_agr LIKE '1-%' or agroquimicos.cod_agr LIKE '$user%')
-    AND cod_agr NOT LIKE '1-1'
-    ORDER BY agroquimicos.cod_agr"; 
-    $result=pg_query($conexion,$sql);
-    while($ver=pg_fetch_row($result)){
-     $recomendaciones = ""; 
-     $datos=$ver[0]."||".
-     $ver[1]."||".
-     $ver[2]."||".
-     $ver[3]."||".
-     $ver[4]."||".
-     $ver[5]."||".
-     $ver[6]."||".
-     $ver[7]."||".
-     $ver[8]."||".
-     $ver[9]."||".
-     $ver[10]."||".
-     $ver[11]."||".
-     $ver[12]."||".
-     $ver[13]."||".
-     $ver[14]."||".
-     $ver[15]."||".
-     $ver[16]."||".
-     $ver[17]."||".
-     $ver[18]."||".
-     $ver[19]."||".
-     $ver[20]."||".
-     $ver[21]."||";
-     ?>
-        <tr>
+        $sql="SELECT agroquimicos.cod_agr, agroquimicos.cod_ins, agroquimicos.nom_agr, 
+        tipo_agroquimico.det_tag, agroquimicos.fun_agr, ingredientes_activos.des_iac, 
+        formulacion.nom_for, formulacion.sig_for, agroquimicos.pcr_agr, agroquimicos.pen_agr, 
+        agroquimicos.pro_agr, unidad_de_medida.des_unm, toxicidad.det_tox, 
+        ingredientes_activos.cod_iac, tipo_agroquimico.cod_tag, formulacion.cod_for,
+        agroquimicos.dos_agr, toxicidad.cod_tox,unidad_de_medida.cod_tum,
+        insumos.cod_ins, insumos.des_ins, agroquimicos.rap_agr
+        FROM agroquimicos
+        INNER JOIN tipo_agroquimico on agroquimicos.cod_tag=tipo_agroquimico.cod_tag
+        INNER JOIN ingredientes_activos on agroquimicos.cod_iac=ingredientes_activos.cod_iac
+        INNER JOIN formulacion on agroquimicos.cod_for=formulacion.cod_for
+        INNER JOIN insumos on agroquimicos.cod_ins=insumos.cod_ins
+        INNER JOIN unidad_de_medida ON insumos.cod_unm=unidad_de_medida.cod_unm
+        INNER JOIN toxicidad ON agroquimicos.cod_tox=toxicidad.cod_tox
+        WHERE (agroquimicos.cod_agr LIKE '1-%' or agroquimicos.cod_agr LIKE '$user%')
+        AND cod_agr NOT LIKE '1-1'
+        ORDER BY agroquimicos.cod_agr"; 
+        $result=pg_query($conexion,$sql);
+        while($ver=pg_fetch_row($result)){
+           $recomendaciones = ""; 
+           $datos=$ver[0]."||".
+           $ver[1]."||".
+           $ver[2]."||".
+           $ver[3]."||".
+           $ver[4]."||".
+           $ver[5]."||".
+           $ver[6]."||".
+           $ver[7]."||".
+           $ver[8]."||".
+           $ver[9]."||".
+           $ver[10]."||".
+           $ver[11]."||".
+           $ver[12]."||".
+           $ver[13]."||".
+           $ver[14]."||".
+           $ver[15]."||".
+           $ver[16]."||".
+           $ver[17]."||".
+           $ver[18]."||".
+           $ver[19]."||".
+           $ver[20]."||".
+           $ver[21]."||";
+           ?>
+           <tr>
 
             <td><?php echo $ver[2] ?></td>
             <td><?php echo $ver[3] ?></td>
@@ -86,34 +86,48 @@ $user =  $_SESSION['idusuario'];
             <td><?php echo $ver[11] ?></td>
             <td><?php echo $ver[12] ?></td>
             <td><?php $sql1="SELECT det_rus from recomendaciones_uso_agr where cod_agr='$ver[0]'";
-                $result1=pg_query($conexion,$sql1);
-                while($ver1=pg_fetch_row($result1)){
-                  echo $ver1[0]; ?>
-                <br><?php 
-                }
-       ?></td>
+            $result1=pg_query($conexion,$sql1);
+            while($ver1=pg_fetch_row($result1)){
+              echo $ver1[0]; ?>
+              <br><?php 
+          }
+          ?></td>
 
-
-
-            <td class="text-right">
-                <div class="dropdown">
-                    <a class="btn btn-sm btn-icon-only" href="#" role="button" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">
-                        <i class="fas fa-ellipsis-v"></i>
+          <td class="text-right">
+              <div class="dropdown">
+                <a class="btn btn-sm btn-icon-only" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="fas fa-ellipsis-v"></i>
+              </a>
+              <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                  <?php  
+                  $partes = explode("-", $ver[0]);
+                  if ($partes[0] != '1') {
+                    ?>
+                    <a class="dropdown-item" href="#" onclick="llenarform(' <?php  echo $datos ?>')">
+                        <div><i class="fas fa-pencil-alt" style="margin-right: 14px;"></i>Editar</div>
                     </a>
-                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                        <a class="dropdown-item" href="#" onclick="llenarform(' <?php  echo $datos ?>')">
-                            <div><i class="fas fa-pencil-alt" style="margin-right: 14px;"></i>Editar</div>
-                        </a>
-                        <a class="dropdown-item" href="#" onclick="eliminar_agroquimico(' <?php  echo $datos ?> ')">
-                            <div><i class="fas fa-times" style="margin-right: 14px;"></i>Eliminar</div>
-                        </a>
-                    </div>
-                </div>
-            </td>
-        </tr>
-        <?php 
-  }
-  ?>
-    </tbody>
+                    <a class="dropdown-item" href="#" onclick="eliminar_agroquimico(' <?php  echo $datos ?> ')">
+                        <div><i class="fas fa-times" style="margin-right: 14px;"></i>Eliminar</div>
+                    </a>
+                    <?php
+                }else{
+                    ?>
+                    <div class="text-center">
+                      <i class="fa fa-ban text-danger" aria-hidden="true" style="font-size: 1.1rem;"></i>
+                  </div>
+
+                  <?php
+              }
+              ?>
+          </div>
+      </div>
+  </td>
+
+</tr>
+<?php 
+}
+?>
+</tbody>
 </table>
+
+
--- php/componentes/componentes_etapas/tab_etapas.php
+++ php/componentes/componentes_etapas/tab_etapas.php
@@ -21,87 +21,103 @@ require '../../conexion.php';
     
     $result=pg_query($conexion,$tab);
     while($ver=pg_fetch_row($result)){  
-        
-    $datos=$ver[0]."||".
-     $ver[1]."||";
+      
+      $datos=$ver[0]."||".
+      $ver[1]."||";
 
-     ?>
-     <tr>
+      ?>
+      <tr>
 
-      <td><center><?php echo $ver[1] ?></center></td>
-      <td>
-      <?php $sql1="SELECT DISTINCT afeccion.nom_afe, etapas_crecimiento.det_eta, 
-                eta_x_afe.ima_eta, eta_x_afe.cod_afe, eta_x_afe.cod_eta
+        <td><center><?php echo $ver[1] ?></center></td>
+        <td>
+          <?php $sql1="SELECT DISTINCT afeccion.nom_afe, etapas_crecimiento.det_eta, 
+          eta_x_afe.ima_eta, eta_x_afe.cod_afe, eta_x_afe.cod_eta
           FROM public.afeccion, public.eta_x_afe, public.etapas_crecimiento
           WHERE afeccion.cod_afe = eta_x_afe.cod_afe
           AND etapas_crecimiento.cod_eta = eta_x_afe.cod_eta
-          AND eta_x_afe.cod_eta = '$ver[0]'";
-            $result1=pg_query($conexion,$sql1);
-            while($ver1=pg_fetch_row($result1)){
-      ?>   
-       <center>
-           <?php echo $ver1[0] ?>
-      </center>
+          AND eta_x_afe.cod_eta = '$ver[0]'  AND (afeccion.cod_afe LIKE '1-%' or afeccion.cod_afe LIKE '$like%')";
+          $result1=pg_query($conexion,$sql1);
+          while($ver1=pg_fetch_row($result1)){
+            ?>   
+            <center>
+             <?php echo $ver1[0] ?>
+           </center>
            <input type="button" name="cargar" class="btn btn-danger sm-3" data-toggle="tooltip"
-                  data-placement="top" title="Eliminar etapa para esta plaga o enfermedad." value="Quitar"
-                  style="font-family:'FontAwesome',tahoma; font-size: 10px;"
-                  onclick="eliminarAsociacion('<?php echo $ver1[3] ?>','<?php echo $ver1[4] ?>')">
+           data-placement="top" title="Eliminar etapa para esta plaga o enfermedad." value="Quitar"
+           style="font-family:'FontAwesome',tahoma; font-size: 10px;"
+           onclick="eliminarAsociacion('<?php echo $ver1[3] ?>','<?php echo $ver1[4] ?>')">
 
 
-       <?php if($ver1[2] != null){?>
-        <center>
-        <a title="<?php echo $ver1[1] ?>" href="#">
+           <?php if($ver1[2] != null){?>
+            <center>
+              <a title="<?php echo $ver1[1] ?>" href="#">
                <img width="90" height="90"
-                   src="../imagenes/<?php echo $ver1[2]?>"
-                   alt="<?php echo $ver1[1] ?>" /></a><br>
-                   </center>
-       <a style="color:#A0D39F">___________________________________________</a><br>
-           
-       <?php 
-      }else{
-          ?>
-       <center>
-           <a title="<?php echo $ver1[1] ?>" href="#">
+               src="../imagenes/<?php echo $ver1[2]?>"
+               alt="<?php echo $ver1[1] ?>" /></a><br>
+             </center>
+             <a style="color:#A0D39F">___________________________________________</a><br>
+             
+             <?php 
+           }else{
+            ?>
+            <center>
+             <a title="<?php echo $ver1[1] ?>" href="#">
                <img width="90" height="90"
-                   src="../imagenes/etapas/sinimagen.jpg"
-                   alt="<?php echo $ver1[1] ?>" /></a>
-                   </center>
-       <a style="color:#A0D39F">_____________________________________________</a><br>
-       
-       <?php
-          }
-        }    
-    ?> 
-    
-      </td>
-      <td><center><input type="button" name="aso_eta"
-                 class="btn btn-success sm-4"
-                 data-placement="top"
-                 title="Asociar Plaga/Enfermedad a Etapa"
-                 value="Asociar a etapa"
-                 style="font-family:'FontAwesome',tahoma; font-size: 10px;"
-                 data-toggle="modal" data-target="#modal-list"
-                 onclick="asociar('<?php echo $ver[0] ?>')"></center></td>
-      <td class="text-right">
-        <div class="dropdown">
-          <a class="btn btn-sm btn-icon-only" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-ellipsis-v"></i>
-          </a>
-          <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-            <a class="dropdown-item" href="#" onclick="modalActualizar(' <?php  echo $datos ?> ')"><div><i class="fas fa-pencil-alt" style="margin-right: 14px;"></i>Editar</div></a>
-            <a class="dropdown-item" href="#" onclick="eliminarEtapa(' <?php  echo $datos ?> ')" ><div><i class="fas fa-times" style="margin-right: 14px;"></i>Eliminar</div></a>
+               src="../imagenes/etapas/sinimagen.jpg"
+               alt="<?php echo $ver1[1] ?>" /></a>
+             </center>
+             <a style="color:#A0D39F">_____________________________________________</a><br>
+             
+             <?php
+           }
+         }    
+         ?> 
+         
+       </td>
+       <td><center><input type="button" name="aso_eta"
+         class="btn btn-success sm-4"
+         data-placement="top"
+         title="Asociar Plaga/Enfermedad a Etapa"
+         value="Asociar a etapa"
+         style="font-family:'FontAwesome',tahoma; font-size: 10px;"
+         data-toggle="modal" data-target="#modal-list"
+         onclick="asociar('<?php echo $ver[0] ?>')"></center></td>
+         <td class="text-right">
+          <div class="dropdown">
+            <a class="btn btn-sm btn-icon-only" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i class="fas fa-ellipsis-v"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+              <?php  
+              $partes = explode("-", $ver[0]);
+              if ($partes[0] != '1') {
+                ?>
+                <a class="dropdown-item" href="#" onclick="modalActualizar(' <?php  echo $datos ?> ')"><div><i class="fas fa-pencil-alt" style="margin-right: 14px;"></i>Editar</div></a>
+                <a class="dropdown-item" href="#" onclick="eliminarEtapa(' <?php  echo $datos ?> ')" ><div><i class="fas fa-times" style="margin-right: 14px;"></i>Eliminar</div></a>
+                <?php
+              }else{
+                ?>
+                <div class="text-center">
+                  <i class="fa fa-ban text-danger" aria-hidden="true" style="font-size: 1.1rem;"></i>
+                </div>
+
+                <?php
+              }
+              ?>
+            </div>
           </div>
-        </div>
-      </td>
-    </tr>
-    <?php 
-  }
-  ?>
-</tbody>
+        </td>
+      </tr>
+      <?php 
+    }
+    ?>
+  </tbody>
 </table>
 
 <script>
-$(function() {
+  $(function() {
     $("[data-toggle='tooltip']").tooltip();
-});
+  });
 </script>
+
+
